0cb5e3e538fee827ee931562b3843d8545e884e7
pakitowka <whitesaint12@gmail.com>
2015-08-26 22:58:56 +0300

    OutOfBoundException. Looking for mistake

--- src/Images.java
+++ src/Images.java
@@ -1,14 +1,11 @@
-import com.sun.javafx.iio.png.PNGDescriptor;
-import sun.awt.image.PNGImageDecoder;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by Andrew on 26.08.2015.
- */
+
 public class Images{
 
     public static void main(String args[]){
@@ -25,8 +22,10 @@ public class Images{
             int height = bufferedImage1.getHeight();
             int width = bufferedImage1.getWidth();
             //i need this variables to solve how much times the loop must do
-            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB );
-            
+           // BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB );
+            //I have created the third image that'll include the differences
+            List<Integer> differencesX = new ArrayList<>();
+            List<Integer> differencesY = new ArrayList<>();
 
             for (int i = 0; i < height; i++){
                 for (int j = 0; i < width; j++){
@@ -34,9 +33,14 @@ public class Images{
                     int pixel2 = bufferedImage2.getRGB( j, i );
 
                     if ((pixel1 < ((pixel2/10)*9)) || (pixel2 <((pixel1/10)*9))){
-
+                        differencesX.add(j);
+                        differencesY.add(i);
                     }
                 }
+            }
+
+            for( int i = 0; i < differencesX.size(); i++){
+                System.out.println(differencesX.get(i) + " " + differencesY.get(i));
             }
         } catch (IOException e) {
             e.printStackTrace();
